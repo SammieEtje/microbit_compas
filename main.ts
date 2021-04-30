3472e39524995b15b3c2247e65d75d126fddd33d
Sander Ettema <Github@dobbe8.nl>
2021-04-30 21:13:31 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let degrees = 0
+input.calibrateCompass()
+basic.showArrow(ArrowNames.North)
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("O")
+    } else if (degrees < 225) {
+        basic.showString("Z")
+    } else if (degrees < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+})
